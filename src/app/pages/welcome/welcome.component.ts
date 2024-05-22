0bb0e2a477f471e1b7d62804b422ed82d4ba3a52
feke9432 <feke9432@gmail.com>
2024-05-23 00:01:35 +0800

    first upload

---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+
+@Component({
+  selector: 'app-welcome',
+  standalone: true,
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.less'],
+})
+export class WelcomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
